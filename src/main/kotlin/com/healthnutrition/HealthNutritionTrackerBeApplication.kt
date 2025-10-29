7a2a74b655a8766a711246f6e7c2f5d05b841a10
Gmlee <gmlee@Gmleeui-MacBookPro.local>
2025-10-29 13:48:54 +0900

    feat: 프로젝트 생성

---
+++ src/main/kotlin/com/healthnutrition/HealthNutritionTrackerBeApplication.kt
@@ -0,0 +1,11 @@
+package com.healthnutrition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealthNutritionTrackerBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<HealthNutritionTrackerBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/healthnutrition/HealthNutritionTrackerBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.healthnutrition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HealthNutritionTrackerBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
