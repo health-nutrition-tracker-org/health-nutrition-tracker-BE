2910baaf6b63915eb2e5e49d5d5d7f1658b4f455
lgm1007 <flxl654@gmail.com>
2025-11-03 20:42:04 +0900

    test: 로그인 API Rest 테스트코드 작성 및 Restdocs 추가

--- src/test/kotlin/com/healthnutrition/domain/account/controller/AccountControllerTest.kt
+++ src/test/kotlin/com/healthnutrition/domain/account/controller/AccountControllerTest.kt
@@ -6,6 +6,7 @@ import com.healthnutrition.domain.account.dto.AccountResponse
 import com.healthnutrition.domain.account.service.AccountService
 import com.healthnutrition.security.config.SecurityConfig
 import com.healthnutrition.security.filter.JwtAuthFilter
+import com.healthnutrition.security.jwt.JwtInfo
 import com.healthnutrition.security.jwt.JwtProvider
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
@@ -76,6 +77,54 @@ class AccountControllerTest : MockMvcTest() {
                     PayloadDocumentation.responseFields(
                         PayloadDocumentation.fieldWithPath("accountId").type(JsonFieldType.NUMBER).description("계정 식별자(ID)"),
                         PayloadDocumentation.fieldWithPath("email").type(JsonFieldType.STRING).description("이메일")
+                    )
+                )
+            )
+    }
+
+    @Test
+    @DisplayName("로그인 테스트")
+    fun signIn_mock_test() {
+        val request = AccountRequest.SignIn(
+            email = "abc@example.com",
+            password = "password1234"
+        )
+        val signInResponse = AccountResponse.SignIn(
+            accountId = 1L,
+            email = "abc@example.com"
+        )
+        val response = JwtInfo(
+            access = "access-token",
+            refresh = "refresh-token",
+            email = "abc@example.com",
+            accountId = 1L,
+            accessExpiredAt = "2023-04-12 10:00:25"
+        )
+
+        every { accountService.signIn(any()) } returns signInResponse
+        every { jwtProvider.issueToken(any()) } returns response
+
+        mockMvc.perform(
+            RestDocumentationRequestBuilders.post("/v1/accounts/tokens")
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(
+                MockMvcRestDocumentation.document(
+                    "Account-Sign-In",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    PayloadDocumentation.requestFields(
+                        PayloadDocumentation.fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                        PayloadDocumentation.fieldWithPath("password").type(JsonFieldType.STRING).description("패스워드")
+                    ),
+                    PayloadDocumentation.responseFields(
+                        PayloadDocumentation.fieldWithPath("access").type(JsonFieldType.STRING).description("JWT access token"),
+                        PayloadDocumentation.fieldWithPath("refresh").type(JsonFieldType.STRING).description("JWT refresh token"),
+                        PayloadDocumentation.fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                        PayloadDocumentation.fieldWithPath("accountId").type(JsonFieldType.NUMBER).description("계정 식별자(ID)"),
+                        PayloadDocumentation.fieldWithPath("accessExpiredAt").type(JsonFieldType.STRING).description("토큰 만료일자")
                     )
                 )
             )
